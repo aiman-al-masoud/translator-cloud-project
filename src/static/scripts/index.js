2c9618c5a3d1c91f63d12af9c163ccd2fec31e80
Davide <davidepio99.lotito@gmail.com>
2022-11-04 16:00:19 +0100

    local cache browser

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -1,7 +1,24 @@
-url = "/translate-api";
+URL = "/translate-api";
+const MAX_CACHE_SIZE = 100;// max number of cache entries in localStorage
 
 function focus_on_input() {
   document.getElementById("from_text").focus();
+}
+
+/**
+ * 
+ * @param {string} string 
+ * @returns {number}
+ */
+function hash(string) {
+    var hash = 0, i, chr;
+    if (string.length === 0) return hash;
+    for (i = 0; i < string.length; i++) {
+      chr = string.charCodeAt(i);
+      hash = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
 }
 
 async function send_text() {
@@ -10,9 +27,21 @@ async function send_text() {
   request.from = document.getElementById('from').value.toLowerCase();
   request.to = document.getElementById('to').value.toLowerCase();
   request.from_text = document.getElementById('from_text').value;
-  request.id = parseInt(1000000000 * Math.random())
+  // request.id = parseInt(1000000000 * Math.random());
 
-  let res = await fetch(url, {
+  request.id = hash(request.from_text);
+
+  if(localStorage.length >= MAX_CACHE_SIZE){
+    localStorage.clear();
+  }
+
+  let cached = localStorage.getItem(request.id)
+  if(cached){
+    document.getElementById('to_text').value = cached
+    return
+  }
+
+  let res = await fetch(URL, {
     method: "POST",
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(request)
@@ -26,7 +55,7 @@ async function send_text() {
   let json = await res.json()
 
   document.getElementById('to_text').value = json.to_text
-
+  localStorage.setItem(request.id, json.to_text)
 }
 
 function switch_lang() {
