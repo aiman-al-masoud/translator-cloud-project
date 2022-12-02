e441cfe71d0be936a2519001242149d903b32614
Davide <davidepio99.lotito@gmail.com>
2022-12-02 19:02:28 +0100

    moving to neo4j db

--- src/db_proxy/app.py
+++ src/db_proxy/app.py
@@ -1,25 +1,18 @@
 import json
 import os
 from flask import Flask, request
-from flask_mysqldb import MySQL
+from neo4j import GraphDatabase
 
 app = Flask(__name__)  # init app
 
 # database connection credentials
-DB_CONFIG = os.path.join(app.root_path,'..', 'config', 'db.json')
-DB_DEFAULT_CONFIG = os.path.join(app.root_path, '..', 'config', 'db-default.json')
-
-if os.path.exists(DB_CONFIG):
-    config = json.loads(open(DB_CONFIG).read())
-elif os.path.exists(DB_DEFAULT_CONFIG):
-    config = json.loads(open(DB_DEFAULT_CONFIG).read())
-else:
-    print('Please add a DB config file in /src/config/db.json !')
-    exit(1)
+URI = "neo4j://localhost:7687"
+user = "neo4j"
+password = "password"
 
 # connection with db
-app.config.update(config)
-mysql = MySQL(app)
+driver = GraphDatabase.driver(URI, auth=(user, password))
+
 OFFSET = 2
 
 def check_json(request):
@@ -37,7 +30,7 @@ def check_json(request):
 
 @app.route('/insert-bad-translation', methods=['POST', 'GET'])
 def insert_bad_translation():
-    '''query to the mysql db for storing the bad translation'''
+    '''query to the db for storing the bad translation'''
 
     if (check:=check_json(request)) != 1:
         return check
@@ -48,22 +41,19 @@ def insert_bad_translation():
         from_text = request.json['from_text']
         to_text = request.json['to_text']
         _id = request.json['id']
+        addr = request.remote_addr
 
         # avoiding to send the response with error 500 (since there is already an equal request saved in the database)
         # users will see "Thank you for your feedback" regardless
-        try:
-            cursor = mysql.connection.cursor()
-            cursor.execute(''' INSERT INTO badTranslations VALUES(%s,%s,%s,%s,%s,1)''',
-                           (fromTag, toTag, from_text, to_text, _id))
-            mysql.connection.commit()
-        except:
-            cursor = mysql.connection.cursor()
-            cursor.execute(''' UPDATE badTranslations SET complaints=complaints+1 WHERE id = (%s)''',
-                           (_id,))  # do not remove "," which is needed to create a tuple
-            mysql.connection.commit()
-        finally:
-            cursor.close()
-            return ""
+
+    with driver.session() as session:
+        query = """CREATE (:User { ip: $ip })-[:REPORTED]->
+                (:BadTranslation { from: $_from, to: $to, from_text: $from_text, to_text: $to_text, from_text: $from_text, id: $id}) 
+                """
+        session.execute_write(lambda tx, ip, fT, tT, ftx, ttx, id: tx.run(query, ip=ip, _from=fT, to=tT, from_text=ftx, to_text=ttx, 
+                id=id), addr, fromTag, toTag, from_text, to_text, _id)
+
+    driver.close()
 
 @app.route('/insert-possible-better-translation', methods=['POST', 'GET'])
 def insert_possible_better_translation():
@@ -73,26 +63,28 @@ def insert_possible_better_translation():
     if request.method == 'POST':
         from_text = request.json['from_text']
         to_text = request.json['to_text']
-        second_id = request.json['secondid']
         fid = request.json['fid']
+        second_id = request.json['secondid']
+        addr = request.remote_addr
+
         if(not from_text.strip() or not to_text.strip()):
             return "Empty from_text or to_text", 400
 
         # avoiding to send the response with error 500 (since there is already an equal request saved in the database)
         # users will see "Thank you for your feedback" regardless
-        try:
-            cursor = mysql.connection.cursor()
-            cursor.execute(''' INSERT INTO possibleBetterTranslations VALUES(%s,%s,%s,%s,0,NOW())''',
-                           (from_text, to_text, second_id, fid))
-            mysql.connection.commit()
-        except Exception as e:
-            cursor = mysql.connection.cursor()
-            cursor.execute(''' UPDATE possibleBetterTranslations SET votes=votes+1 WHERE secondid = (%s)''',
-                           (second_id,))  # do not remove "," which is needed to create a tuple
-            mysql.connection.commit()
-        finally:
-            cursor.close()
-            return ""
+    
+    with driver.session() as session:
+        query = """CREATE (:User { ip: $ip })-[:PROPOSED]->(:BetterTranslation {from_text: $from_text, to_text: $to_text, id: $id});
+                    MATCH (bad:BadTranslation)
+                    MATCH (better:BetterTranslation)
+                    WHERE bad.id = $fid
+                    MERGE (bad)-[:IMPROVED_BY]->(better)
+                    RETURN *
+                """
+        session.execute_write(lambda tx, ip, ftx, ttx, id, fid: tx.run(query, ip=ip, from_text=ftx, to_text=ttx, 
+            id=id, fid=fid), addr, from_text, to_text, second_id, fid)
+    #TODO: counts the number of votes for each better translation
+    driver.close()
 
 @app.route('/read-bad-translations', methods=['POST', 'GET'])
 def read_bad_translations():
@@ -101,17 +93,9 @@ def read_bad_translations():
 
     if request.method == 'POST':
         page =  request.json['page']
-
-        try:
-            cursor = mysql.connection.cursor()
-            cursor.execute(''' SELECT * FROM badTranslations ORDER BY complaints DESC LIMIT %s OFFSET %s''', (OFFSET, OFFSET*page,)) # multipled by 2
-            data = cursor.fetchall()
-        except Exception as e:
-            print("ERROR: Query exception:", e)
-        finally:
-            cursor.close()
-
-    return json.dumps(data)
+    
+    #TODO
+    pass
 
 @app.route('/read-possible-better-translation-by-id', methods=['POST', 'GET'])
 def read_possible_better_translation_by_id():
@@ -121,18 +105,9 @@ def read_possible_better_translation_by_id():
     if request.method == 'POST':
         id_prop =  request.json['id_prop']
         page =  request.json['page']
-
-        try:
-            cursor = mysql.connection.cursor()
-            cursor.execute(''' SELECT from_text, to_text, secondid, fid, votes FROM possibleBetterTranslations WHERE fid = (%s) 
-            ORDER BY votes DESC, timestamp LIMIT %s OFFSET %s''', (id_prop, OFFSET, OFFSET*page,))
-            data = cursor.fetchall()
-        except Exception as e:
-            print("ERROR: Query exception:", e)
-        finally:
-            cursor.close()
-
-    return json.dumps(data)
+    
+    #TODO
+    pass
 
 @app.route('/vote-possible-better-translation', methods=['POST', 'GET'])
 def vote_possible_better_translation():
@@ -142,17 +117,9 @@ def vote_possible_better_translation():
     if request.method == 'POST':
         second_id = request.json['secondid']
         operation = request.json["operation"]
-        try:
-            cursor = mysql.connection.cursor()
-
-            cursor.execute(''' UPDATE possibleBetterTranslations SET votes=votes+(%s) WHERE secondid = (%s)''',
-                           (operation,second_id,))  # do not remove "," which is needed to create a tuple
-            mysql.connection.commit()
-        except Exception as e:
-            print("ERROR: Query votes exception:", e)
-        finally:
-            cursor.close()
-            return ""
+    
+    #TODO
+    pass
 
 # added for running the server directly with the run button
 app.run(host='localhost', port=8080)
